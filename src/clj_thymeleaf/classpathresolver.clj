30262002066faece14ccbab984f7c96e795603a6
plakat <github@plak.at>
2014-12-06 16:22:10 +0100

    debugging after namespace changes

---
+++ src/clj_thymeleaf/classpathresolver.clj
@@ -0,0 +1,24 @@
+(ns
+  ^{:author muecke}
+  gbquery.util.classpathresolver
+  (:import java.io.InputStream
+           org.thymeleaf.resourceresolver.IResourceResolver
+           org.thymeleaf.TemplateProcessingParameters
+           )
+  (:require [clojure.java.io :as io])
+  )
+
+(gen-class
+  :name gbquery.util.ClasspathResolver
+  :implements [org.thymeleaf.resourceresolver.IResourceResolver]
+  )
+
+(defn- -getName [this]
+  "CLASSPATH")
+
+(defn- -getResourceAsStream [this params name]
+  (println "classpathresolver looking for template " name)
+  (when-let [resource-data (io/resource name)]
+    (io/input-stream resource-data)
+    )
+  )
