987d17242b9ac35b871899736ccb35a9ae357883
plakat <github@plak.at>
2014-12-06 14:12:38 +0100

    cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-thymeleaf "0.1.0-SNAPSHOT"
   :description "Clojure integration for Thymeleaf templating"
-  :url "https://example.com/FIXM://github.com/plakat/clj-thymeleaf"
+  :url "https://github.com/plakat/clj-thymeleaf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
